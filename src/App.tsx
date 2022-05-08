8fba446bc690b65beb43de1191c84b1ec2804c8e
Andrewwxo <74658753+Andrewwxo@users.noreply.github.com>
2022-05-08 20:40:52 +0300

    app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import {MyPosts} from './components/Profile/MyPosts/MyPosts';
+import {Header} from './components/Header/Header';
+import {Navbar} from './components/Navbar/Navbar';
+import {Profile} from './components/Profile/Profile';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
